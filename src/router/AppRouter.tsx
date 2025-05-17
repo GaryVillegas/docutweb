df53e19c27d796a7a06f42d8f56ce9e4d39a87b3
GaryVillegas <gary.villegasb@gmail.com>
2025-05-16 22:23:39 -0400

    example

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../views/Home";
+import { Login } from "../views/auth/Login";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
