81545d7241dc7b07529d90c2063fcd17e9c9d6be
GaryVillegas <gary.villegasb@gmail.com>
2025-06-17 22:51:07 -0400

    feat[UserInfo]: se avanza en user info

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,6 +4,7 @@ import { Login } from "../views/auth/Login";
 import { Home } from "../views/Home";
 import { Register } from "../views/auth/Register";
 import PrivateRoute from "../views/auth/PrivateRoute";
+import { UserInfo } from "../views/auth/UserInfo";
 
 export const AppRouter = () => {
   return (
@@ -11,6 +12,14 @@ export const AppRouter = () => {
       <Route path="/" element={<Web />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route
+        path="/UserInfo"
+        element={
+          <PrivateRoute>
+            <UserInfo />
+          </PrivateRoute>
+        }
+      />
       <Route
         path="/home"
         element={
---
+++ src/views/auth/UserInfo.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import styles from "./auth.module.css";
+import { Steps, Spin } from "antd";
+import { useMessageContext } from "../../context/MessageContext";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import authenticationService from "../../services/authentication.service";
+
+export const UserInfo = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const { showMessage } = useMessageContext();
+  const navigate = useNavigate();
+  const { user, loading } = useAuth();
+
+  const next = () => {
+    setCurrentSlide(currentSlide + 1);
+  };
+  const prev = () => {
+    setCurrentSlide(currentSlide - 1);
+  };
+
+  const FooterButtons = () => {
+    return (
+      <div>
+        {currentSlide < steps.length - 1 && (
+          <button onClick={() => next()}>Siguiente</button>
+        )}
+        {currentSlide === steps.length - 1 && (
+          <button onClick={() => showMessage("All done guys!", "success")}>
+            Terminar
+          </button>
+        )}
+        {currentSlide > 0 && <button onClick={() => prev()}>Volver</button>}
+        {currentSlide === 0 && (
+          <button onClick={() => cancel(user)}>Cancelar</button>
+        )}
+      </div>
+    );
+  };
+
+  const cancel = async (user: any) => {
+    try {
+      await authenticationService.deleteAccount(user);
+      navigate("/register");
+    } catch (error) {
+      showMessage(`${error}`, "error");
+    }
+  };
+
+  const addUserInfo = () => {
+    return (
+      <>
+        <div>Información de usuario :)</div>
+      </>
+    );
+  };
+
+  const storeInfo = () => {
+    return <div>Información de tienda</div>;
+  };
+
+  const chooseCategory = () => {
+    return <div>Categorias de tienda</div>;
+  };
+
+  const steps = [
+    { title: "Tu información", content: addUserInfo },
+    { title: "Infórmanos sobre tu negocio", content: storeInfo },
+    { title: "Categorías", content: chooseCategory },
+  ];
+  const items = steps.map((item) => ({ key: item.title, title: item.title }));
+  if (loading) return <Spin fullscreen={true} size="large" />;
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.containerUserInfo}>
+        <Steps size="default" current={currentSlide} items={items} />
+        <FooterButtons />
+      </div>
+    </div>
+  );
+};
